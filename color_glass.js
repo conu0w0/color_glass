b4829f122a150d300390d3e76ac2c28847798abb
conu0w0 <h010185@smsh.ntpc.edu.tw>
2025-06-11 02:24:22 +0800

    Update color_glass.js

--- color_glass.js
+++ color_glass.js
@@ -479,7 +479,7 @@ function draw_selected(){
     var cx = Math.floor(selected%xmax);
     var cy = Math.floor(selected/xmax);
     counter++;
-    cel[cy][cx].y = cel[cy][cx].oy - 14*Math.sin(Math.PI*counter/12);
+    cel[cy][cx].y = cel[cy][cx].oy - 14*Math.sin(Math.PI*counter/16);
     draw_request = true;
 }
 
